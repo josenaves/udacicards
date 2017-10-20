3a5a89753a5be6937bd1e2f076d19352c6314937
José Naves Moura Neto <josenaves@gmail.com>
2017-10-19 22:42:32 -0200

    Add TextButton component

---
+++ src/components/TextButton.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Text, TouchableOpacity, StyleSheet } from 'react-native';
+import { purple } from '../colors';
+
+const styles = StyleSheet.create({
+  reset: {
+    textAlign: 'center',
+    color: purple,
+  },
+});
+
+export default function TextButton({ children, onPress, style = {} }) {
+  return (
+    <TouchableOpacity onPress={onPress}>
+      <Text style={[styles.reset, style]}>{children}</Text>
+    </TouchableOpacity>
+  );
+}
