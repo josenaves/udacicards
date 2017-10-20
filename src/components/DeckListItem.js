d2628f8071159f834ff344c730436366345612b7
José Naves Moura Neto <josenaves@gmail.com>
2017-10-19 22:45:50 -0200

    Add deck list item

---
+++ src/components/DeckListItem.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { StyleSheet, View, Text, TouchableHighlight } from 'react-native';
+import { observer, inject } from 'mobx-react';
+import { lightgrey } from '../colors';
+
+const styles = StyleSheet.create({
+  item: {
+    padding: 40,
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  deckHeader: {
+    fontSize: 26,
+  },
+  deckSubheader: {
+    fontSize: 16,
+  },
+});
+
+@inject('store')
+@observer
+export default class DeckListItem extends React.PureComponent {
+  render() {
+    const { title, subTitle, onPressItem } = this.props;
+    return (
+      <TouchableHighlight
+        onPress={() => onPressItem()}
+        activeOpacity={0.7}
+        underlayColor={lightgrey}
+      >
+        <View style={styles.item} {...this.props}>
+          <Text style={styles.deckHeader}>{title}</Text>
+          <Text style={styles.deckSubheader}>{subTitle}</Text>
+        </View>
+      </TouchableHighlight>
+    );
+  }
+}
