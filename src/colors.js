c8ae836c4e3e04711734a62d31cfad9bf5c633d9
José Naves Moura Neto <josenaves@gmail.com>
2017-10-19 22:42:07 -0200

    Add lightgrey color

--- src/colors.js
+++ src/colors.js
@@ -1,8 +1,10 @@
 export const purple = '#292477';
 export const gray = '#757575';
 export const white = '#fff';
+export const black = '#000';
 export const red = '#b71845';
 export const orange = '#f26f28';
 export const blue = '#4e4cb8';
-export const lightPurp = '#7c53c3';
+export const lightPurple = '#7c53c3';
 export const pink = '#b93fb3';
+export const lightgrey = '#d3d3d3';
