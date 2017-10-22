b40a0b4ffa6831a9eaad8d2dd5f1fc47c0b5a9a0
José Naves Moura Neto <josenaves@gmail.com>
2017-10-22 15:01:15 -0200

    Add new deck on store

--- src/components/AddDeck.js
+++ src/components/AddDeck.js
@@ -1,43 +1,37 @@
-import React from 'react';
-import { StyleSheet, View, Text, TextInput } from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, View, Text, TextInput, Alert } from 'react-native';
 import { observer, inject } from 'mobx-react';
 import TextButton from './TextButton';
 import { white, black } from '../colors';
+import { saveDeckTitle } from '../helpers/storageHelper';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+    margin: 10,
   },
-  titleContainer: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+  deckTitle: {
+    fontSize: 50,
+    textAlign: 'center',
   },
-  buttonsContainer: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  deckHeader: {
-    fontSize: 60,
-  },
-  deckSubheader: {
-    fontSize: 30,
+  inputDeck: {
+    marginTop: 30,
+    width: '90%',
+    height: 50,
+    fontSize: 18,
+    borderColor: black,
+    borderWidth: 0.5,
   },
   button: {
+    marginTop: 30,
     fontSize: 25,
     width: 200,
-    margin: 10,
     padding: 15,
     borderColor: black,
     borderRadius: 6,
     borderWidth: 0.5,
-  },
-  buttonWhite: {
-    backgroundColor: white,
-    color: black,
   },
   buttonBlack: {
     backgroundColor: black,
@@ -47,13 +41,45 @@ const styles = StyleSheet.create({
 
 @inject('store')
 @observer
-export default class AddDeck extends React.Component {
+export default class AddDeck extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { deckTitle: '' };
+  }
+
+  saveDeck = async () => {
+    const { navigate } = this.props.navigation;
+    const { deckTitle } = this.state;
+    const { store } = this.props;
+
+    if (!deckTitle) {
+      Alert.alert('Warning', 'Deck title cannot be empty !');
+      return;
+    }
+
+    if (store.deckTitles.includes(deckTitle)) {
+      Alert.alert('Warning', 'Deck title already exists !');
+      return;
+    }
+
+    await saveDeckTitle(deckTitle);
+    this.setState({ deckTitle: '' });
+    navigate('DeckList');
+  };
+
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.deckHeader}>What is the title of your new deck?</Text>
-        <TextInput style={{}} placeholder="Type deck name!" />
-        <TextButton style={[styles.button, styles.buttonWhite]}>Submit</TextButton>
+        <Text style={styles.deckTitle}>What is the title of your new deck?</Text>
+        <TextInput
+          style={styles.inputDeck}
+          placeholder="Type deck title"
+          onChangeText={text => this.setState({ deckTitle: text })}
+          value={this.state.deckTitle}
+        />
+        <TextButton style={[styles.button, styles.buttonBlack]} onPress={this.saveDeck}>
+          Submit
+        </TextButton>
       </View>
     );
   }
